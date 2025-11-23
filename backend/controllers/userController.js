a4f3451e9fbeb59b31fc61d6f48b706b294d3716
Kumar manak <manakkash20@gmail.com>
2025-11-23 16:41:58 +0530

    usercontroller.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,26 @@
-const test = (req, res) => {
-  res.json({ message: 'API is working!' });
-};
+const User = require('../models/User')
 
-module.exports = { test };
+exports.getUser = async (req, res) => {
+    const user = await User.findById(req.params.id).select('-password')
+    res.json(user)
+}
+
+exports.updateUser = async (req, res) => {
+    const updates = req.body
+    const user = await User.findByIdAndUpdate(req.user.id, updates, { new: true }).select('-password')
+    res.json(user)
+}
+
+exports.follow = async (req, res) => {
+    const { id } = req.params
+    await User.findByIdAndUpdate(req.user.id, { $push: { following: id } })
+    await User.findByIdAndUpdate(id, { $push: { followers: req.user.id } })
+    res.json({ message: 'Followed' })
+}
+
+exports.unfollow = async (req, res) => {
+    const { id } = req.params
+    await User.findByIdAndUpdate(req.user.id, { $pull: { following: id } })
+    await User.findByIdAndUpdate(id, { $pull: { followers: req.user.id } })
+    res.json({ message: 'Unfollowed' })
+}
